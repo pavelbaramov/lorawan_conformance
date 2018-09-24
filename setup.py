87dfc04fcc540e8dd01b9c998873edc6b741e916
Pablo Modernell <pablomodernell@gmail.com>
2018-09-24 18:29:21 +0200

    Updates requirements, using pika 0.12.0. Adds description to Agent package in PyPI.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 MAJOR = 1
 MINOR = 0
-PATCH = 0
+PATCH = 7
 VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
 
 name = 'florawan_testing'
-description = "F-LoRa LoRaWAN testing user Agent: modules and programs for testing PyPI"
+description = "F-LoRa LoRaWAN testing user Agent."
 
 
 CLASSIFIERS = [
@@ -16,6 +16,8 @@ CLASSIFIERS = [
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Internet",
     "Topic :: Software Development :: Testing",
     "Topic :: Scientific/Engineering",
@@ -31,13 +33,20 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name=name,
+      author="Pablo Modernell",
+      author_email="pablomodernell@gmail.com",
+      mantainer="Pablo Modernell",
+      mantainer_email="pablomodernell@gmail.com",
+      description=description,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       version=VERSION,
       packages=find_packages(),
       py_modules=['utils', 'message_queueing'],
       include_package_data=True,
       install_requires=[
           'click==6.7',
-          'pika==0.10.0',
+          'pika==0.12.0',
           'pycryptodomex==3.6.6'
       ],
       entry_points={
