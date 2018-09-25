64c1659232af1f100abc93a626a191203654fc40
Pablo Modernell <pablomodernell@gmail.com>
2018-09-25 13:29:59 +0200

    Sets v1.0.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 MAJOR = 1
 MINOR = 0
-PATCH = 7
+PATCH = 0
 VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
 
 name = 'florawan_testing'
@@ -15,6 +15,7 @@ CLASSIFIERS = [
     "Intended Audience :: Developers",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
